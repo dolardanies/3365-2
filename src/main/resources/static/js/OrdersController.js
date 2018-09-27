5bc2cca474a688b0c1d0b5f735987f879fe702aa
d.castiblanco <dolardanies@hotmail.com>
2018-09-27 09:36:02 -0500

    Metodos implementados

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -94,12 +94,52 @@ var OrdersControllerModule = (function () {
     var addItemToOrder = function (orderId, item) {
         // todo implement
     };
+    
+    var loadSelectOrdersData = function (){
+        var callback = {
+            onSuccess: function(ordersList){
+                $("#orders").empty();
+                for(order in ordersList){
+                    $("#orders").append("<option value='"+order+"'>Table "+order);
+                }
+            },
+            onFailed: function(exception){
+                console.log(exception);
+                errorMessage();
+            }
+        }
+        RestControllerModule.getOrders(callback);
+    };
+    
+    var showSelectedOrder = function(){
+        var s = document.getElementById("orders");
+        var selected = s.options[s.selectedIndex].value;
+        console.log(selected);
+        var callback = {
+            onSuccess: function(order){
+                console.log(order);
+                $("#actualOrder").empty();
+                $("#actualOrder").append("<thead> <tr>  <th scope='col'>Item</th> <th scope='col'>Quantity</th> <th scope='col'></th> <th scope='col'></th>  </tr> </thead>");
+                for(dish in order[selected].orderAmountsMap){
+                    $("#actualOrder").append("<tbody> <tr> <td>"+dish+"</td> <td>"+order[selected].orderAmountsMap[dish]+"</td> <td><button type='button' class='btn'>Update</button></td> <td><button type='button' class='btn'>Delete</button></td></tr> </tbody>");
+                }
+            },
+            onFailed: function(exception){
+                console.log(exception);
+                errorMessage();
+            }
+        }
+        RestControllerModule.getOrderById(selected,callback);
+    }
+    
 
     return {
         showOrdersByTable: showOrdersByTable,
         updateOrder: updateOrder,
         deleteOrderItem: deleteOrderItem,
-        addItemToOrder: addItemToOrder
+        addItemToOrder: addItemToOrder,
+        loadSelectOrdersData: loadSelectOrdersData,
+        showSelectedOrder: showSelectedOrder
     };
 
 })();
--- src/main/resources/static/js/RestaurantRestController.js
+++ src/main/resources/static/js/RestaurantRestController.js
@@ -1,32 +1,55 @@
 var RestControllerModule = (function () {
 
-  var getOrders = function (callback) {
-    axios.get('/orders')
-        .then(function (response) {
-          callback.onSuccess(response.data);
-        })
-        .catch(function (reason) {
-          callback.onFailed(reason);
-        });
-  };
+    var getOrders = function (callback) {
+        axios.get('/orders')
+                .then(function (response) {
+                    callback.onSuccess(response.data);
+                })
+                .catch(function (reason) {
+                    callback.onFailed(reason);
+                });
+    };
 
-  var updateOrder = function (order, callback) {
-    // todo implement
-  };
+    var getOrderById = function (orderId, callback) {
+        axios.get('/orders/' + orderId)
+                .then(function (response) {
+                    callback.onSuccess(response.data);
+                })
+                .catch(function (reason) {
+                    callback.onFailed(reason);
+                });
+    }
 
-  var deleteOrder = function (orderId, callback) {
-    // todo implement
-  };
+    var updateOrder = function (order, callback) {
+        axios.put('/orders/'+actual,order)
+            .then(function(){
+                callback.onSuccess();
+            })
+            .catch(function(reason){
+                callback.onFailed(reason);
+            });
+    };
 
-  var createOrder = function (order, callback) {
-    // todo implement
-  };
+    var deleteOrder = function (orderId, callback) {
+        axios.delete('/orders/'+orderId)
+            .then(function(){
+                callback.onSuccess();
+            })
+            .catch(function(reason){
+                callback.onFailed(reason);
+            });
+    };
 
-  return {
-    getOrders: getOrders,
-    updateOrder: updateOrder,
-    deleteOrder: deleteOrder,
-    createOrder: createOrder
-  };
+    var createOrder = function (order, callback) {
+        // todo implement
+    };
+
+    return {
+        getOrders: getOrders,
+        getOrderById: getOrderById,
+        updateOrder: updateOrder,
+        deleteOrder: deleteOrder,
+        createOrder: createOrder
+    };
 
 })();
