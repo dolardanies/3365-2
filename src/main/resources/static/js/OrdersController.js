5aadb33c962b61753b20fd898f11d7651fc0af08
d.castiblanco <dolardanies@hotmail.com>
2018-09-27 10:04:48 -0500

    Agregar producto

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -56,23 +56,9 @@
  }*/
 
 var OrdersControllerModule = (function () {
-    
+
     var selectedOrder;
-    /* var getActualOrder = function () {
-        var s = document.getElementById("orders");
-        var selected = s.options[s.selectedIndex].value;
-        var callback = {
-            onSuccess: function (orden) {
-                return orden;
-            },
-            onFailed: function (error) {
-                console.log(error);
-                errorMessage();
-            }
-        }
-        RestControllerModule.getOrderById(selected, callback);
-    }*/
-    
+
     var errorMessage = function () {
         alert("Hay un problema con nuestros servidores. Pedimos disculpas por la inconveniencia, intente de nuevo más tarde");
     }
@@ -88,7 +74,7 @@ var OrdersControllerModule = (function () {
                     for (map in ordersList[key].orderAmountsMap) {
                         $("#Table" + key).append("<tbody> <tr> <td>" + map + "</td> <td>" + orders[key].orderAmountsMap[map] + "</td> </tr> </tbody>");
                     }
-                    $("#actualT").append("</table>");
+                    //$("#actualT").append("</table>");
                 }
 
             },
@@ -101,15 +87,6 @@ var OrdersControllerModule = (function () {
     };
 
     var updateOrder = function () {
-        // todo implement
-    };
-
-    var deleteOrderItem = function (itemName) {
-        // todo implement
-    };
-
-    var addItemToOrder = function (orderId, item) {
-        selectedOrder[orderId].orderAmountsMap[item[0]] = parseInt(item[1]);
         var callback = {
             onSuccess: function () {
                 showSelectedOrder();
@@ -119,46 +96,72 @@ var OrdersControllerModule = (function () {
                 errorMessage();
             }
         }
-        RestControllerModule.updateOrder(selectedOrder[orderId],callback);
+        RestControllerModule.updateOrder(selectedOrder[orderId], callback);
     };
-    
-    var loadSelectOrdersData = function (){
+
+    var deleteOrderItem = function (itemName) {
+        // todo implement
+    };
+
+    var addItemToOrder = function (orderId, item) {
+        var name = item[0];
+        var quantity = item[1];
+        if(Object.keys(selectedOrder[orderId].orderAmountsMap).includes(name)){
+            selectedOrder[orderId].orderAmountsMap[name] += parseInt(quantity);
+        } else {
+            selectedOrder[orderId].orderAmountsMap[item[0]] = parseInt(item[1]);
+        }
         var callback = {
-            onSuccess: function(ordersList){
+            onSuccess: function () {
+                showSelectedOrder();
+            },
+            onFailed: function (reason) {
+                console.log(reason);
+                errorMessage();
+            }
+        }
+        RestControllerModule.updateOrder(selectedOrder[orderId], callback);
+    };
+
+    var loadSelectOrdersData = function () {
+        var callback = {
+            onSuccess: function (ordersList) {
                 $("#orders").empty();
-                for(order in ordersList){
-                    $("#orders").append("<option value='"+order+"'>Table "+order);
+                for (order in ordersList) {
+                    $("#orders").append("<option value='" + order + "'>Table " + order);
                 }
             },
-            onFailed: function(exception){
+            onFailed: function (exception) {
                 console.log(exception);
                 errorMessage();
             }
         }
         RestControllerModule.getOrders(callback);
     };
-    
-    var showSelectedOrder = function(){
+
+    var showSelectedOrder = function () {
         var s = document.getElementById("orders");
         var selected = s.options[s.selectedIndex].value;
-        
+
         var callback = {
-            onSuccess: function(order){
+            onSuccess: function (order) {
                 selectedOrder = order;
                 $("#actualOrder").empty();
                 $("#actualOrder").append("<thead> <tr>  <th scope='col'>Item</th> <th scope='col'>Quantity</th> <th scope='col'></th> <th scope='col'></th>  </tr> </thead>");
-                for(dish in order[selected].orderAmountsMap){
-                    $("#actualOrder").append("<tbody> <tr> <td>"+dish+"</td> <td>"+order[selected].orderAmountsMap[dish]+"</td> <td><button type='button' class='btn'>Update</button></td> <td><button type='button' class='btn'>Delete</button></td></tr> </tbody>");
+                for (dish in order[selected].orderAmountsMap) {
+                    $("#actualOrder").append("<tbody> <tr> <td> <input id='item' type='text' value='"+dish+"'></td> <td> <input id='item' type='text' value='"+order[selected].orderAmountsMap[dish]+"'> </td> <td><button type='button' class='btn'>Update</button></td> <td><button type='button' class='btn'>Delete</button></td></tr> </tbody>");
                 }
             },
-            onFailed: function(exception){
+            onFailed: function (exception) {
                 console.log(exception);
                 errorMessage();
             }
+
+             
         }
-        RestControllerModule.getOrderById(selected,callback);
+        RestControllerModule.getOrderById(selected, callback);
     }
-    
+
 
     return {
         showOrdersByTable: showOrdersByTable,
